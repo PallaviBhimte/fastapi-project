5731c28d89220dfaa0ad0a8a77b25024bcc4e20d
PallaviBhimte <p28bhimte@gmail.com>
2026-06-22 13:06:27 +1000

    retrieve products from name search

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -1,16 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+from service.products import get_all_products
 
+# Create FastAPI application
 app = FastAPI()
 
-# Static Route: Calling this 40 times will give the message 40 times.
+# Route path GET endpoint
 @app.get('/')
 def root():
     return {"message": "Welcome to FastAPI."}
 
-# Dynamic Route - Calling product data based on the ID.
-@app.get('/products/{id}')
-def get_products(id:int):
-    products = ['Brush', 'Laptop', 'Mouse', 'Monitor']
-    return products[id]
+# # Get all products
+# @app.get("/products")
+# def get_products():
+#     return get_all_products()
 
-# whatever ID you pass, you'll get the data according to that.
+# Get list of products from name-based Search filter
+@app.get("/products")
+def list_products(
+    name:str = Query(
+        default = None, 
+        min_length = 1, 
+        max_length = 50, 
+        description = "Search by Product Name (case insensitive)"
+    )
+):
+    # Load all products from data
+    products = get_all_products()
+
+    if name:
+        # clean name
+        search_term = name.strip().lower()
+
+        # Filter & list products after matching search term and product name
+        products = [p for p in products if search_term in p.get("name", "").lower()]
+
+        # Raise error if no products match the search term
+        if not products:
+            raise HTTPException(
+                status_code = 404,
+                detail = f"No product found matching name = {name}"
+            )
+        
+        # Count final results after filtering
+        total = len(products)
+
+    return {"total": total, "items": products}
